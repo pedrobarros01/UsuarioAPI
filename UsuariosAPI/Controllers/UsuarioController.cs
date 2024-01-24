247335f532f0a4f6ccc7e83da31fb887f30e01a5
Pedro Barros <pedrobarros0101@outlook.com>
2024-01-24 01:48:57 -0300

    Adicionar arquivos de projeto.

---
+++ UsuariosAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsuariosAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsuarioController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult CadastraUsuario()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ UsuariosAPI/Data/DTOs/CreateUsuarioDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsuariosAPI.Data.DTOs;
+
+public class CreateUsuarioDTO
+{
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    public DateTime DataNascimento { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [Required]
+    [Compare("Password")]
+    public string ConfirmPassword { get; set; }
+}
